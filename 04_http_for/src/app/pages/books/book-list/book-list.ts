7e90442c8ccade48aedb375160731bd17225c2be
potapchukdmytro <traiget@gmail.com>
2026-08-20 21:00:29 +0300

    book list page

--- 04_http_for/src/app/pages/books/book-list/book-list.ts
+++ 04_http_for/src/app/pages/books/book-list/book-list.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { BooksService } from '../../../services/books/books-service';
 import { Book } from '../../../services/types';
+import { HryvniaPipe } from '../../../pipes/hryvnia-pipe';
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
+  imports: [HryvniaPipe],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
 export class BookList implements OnInit {
   private booksService = inject(BooksService);
 
-  books = signal<Book[]>([]);
+  public books = signal<Book[]>([]);
 
   // Спрацює тільки при першому малюванні компонента
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.booksService.getBooks().subscribe((data) => {
       this.books.set(data.payload.items);
-      console.log(data);
     });
+  }
+
+  public imageErrorHandle(event: ErrorEvent) {
+    const img = event.target as HTMLImageElement;
+    img.src = "/images/bookDefault.png";
   }
 }
