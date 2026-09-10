64c1e185726c9e10e56fbf7debce32374ccfdb08
potapchukdmytro <traiget@gmail.com>
2026-09-10 21:05:30 +0300

    profile

--- 10_email/src/app/pages/user/profile/profile.ts
+++ 10_email/src/app/pages/user/profile/profile.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { AuthService } from '../../../services/auth/auth-service';
+import { CookieService } from '../../../services/cookie/cookie-service';
+import { Router } from '@angular/router';
+import { User } from '../../../services/types';
 
 @Component({
   selector: 'app-profile',
@@ -6,4 +10,28 @@ import { Component } from '@angular/core';
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {}
+export class Profile implements OnInit {
+  private authService = inject(AuthService);
+  private cookieService = inject(CookieService);
+  private router = inject(Router);
+
+  user = signal<User | null>(null);
+
+  ngOnInit(): void {
+    const token = this.cookieService.get('ujt');
+    if (!token) {
+      this.authService.logout();
+      this.router.navigate(['/login']);
+      return;
+    }
+    
+
+    this.authService.getUser(token).subscribe({
+      next: (data) => { this.user.set(data.payload); },
+      error: (error) => {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+      },
+    });
+  }
+}
--- 10_email/src/app/services/auth/auth-service.ts
+++ 10_email/src/app/services/auth/auth-service.ts
@@ -1,73 +1,84 @@
 import { Service, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ApiResponse, JwtPayload, Login, Register } from '../types';
+import { ApiResponse, JwtPayload, Login, Register, User } from '../types';
 import { jwtDecode } from 'jwt-decode';
 import { CookieService } from '../cookie/cookie-service';
 
 @Service()
 export class AuthService {
-    private httpClient = inject(HttpClient);
-    private cookieService = inject(CookieService);
-    private baseUrl = 'https://frontend53.somee.com/api/auth/';
+  private httpClient = inject(HttpClient);
+  private cookieService = inject(CookieService);
+  private baseUrl = 'https://frontend53.somee.com/api/auth/';
 
-    isAuth = signal(false);
+  isAuth = signal(false);
 
-    constructor() {
-        const token = this.cookieService.get('ujt');
-        token ? this.isAuth.set(true) : this.isAuth.set(false);
+  constructor() {
+    const token = this.cookieService.get('ujt');
+    token ? this.isAuth.set(true) : this.isAuth.set(false);
+  }
+
+  loginRequest(data: Login) {
+    return this.httpClient.post<ApiResponse<string>>(this.baseUrl + 'login', data);
+  }
+
+  registerRequest(data: Register) {
+    return this.httpClient.post<ApiResponse<string>>(
+      this.baseUrl + 'register?callbackUrl=http://localhost:4200/email/confirm',
+      data,
+    );
+  }
+
+  confirmEmailRequest(userId: string, token: string) {
+    return this.httpClient.get<ApiResponse<null>>(
+      this.baseUrl + `confirmEmail?token=${token}&userId=${userId}`,
+    );
+  }
+
+  getUser(token: string) {
+    const json = JSON.stringify(token);
+    return this.httpClient.post<ApiResponse<User>>(this.baseUrl + 'me', json, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  login(token: string) {
+    this.isAuth.set(true);
+    try {
+      const decodedToken = jwtDecode(token);
+      const tokenExp = decodedToken.exp;
+      if (tokenExp) {
+        this.cookieService.setUnix('ujt', token, tokenExp);
+      }
+    } catch (error) {}
+  }
+
+  logout() {
+    this.isAuth.set(false);
+    this.cookieService.remove('ujt');
+  }
+
+  getLocalUser() {
+    const token = this.cookieService.get('ujt');
+    if (!token) {
+      return null;
     }
 
-    loginRequest(data: Login) {
-        return this.httpClient.post<ApiResponse<string>>(this.baseUrl + 'login', data);
+    try {
+      const user = jwtDecode<JwtPayload>(token);
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  isAdmin() {
+    const user = this.getLocalUser();
+    if (user) {
+      return user.role == 'admin';
     }
 
-    registerRequest(data: Register) {
-        return this.httpClient.post<ApiResponse<string>>(this.baseUrl + 'register?callbackUrl=http://localhost:4200/email/confirm', data);
-    }
-
-    confirmEmailRequest(userId: string, token: string) {
-        return this.httpClient.get<ApiResponse<null>>(this.baseUrl + `confirmEmail?token=${token}&userId=${userId}`);
-    }
-
-    login(token: string) {
-        this.isAuth.set(true);
-        try {
-            const decodedToken = jwtDecode(token);
-            const tokenExp = decodedToken.exp;
-            if(tokenExp) {
-                this.cookieService.setUnix('ujt', token, tokenExp);
-            }
-            
-        } catch (error) {
-            
-        }
-    }
-
-    logout() {
-        this.isAuth.set(false);
-        this.cookieService.remove('ujt');
-    }
-
-    getLocalUser() {
-        const token = this.cookieService.get('ujt');
-        if(!token) {
-            return null;
-        }
-
-        try {
-            const user = jwtDecode<JwtPayload>(token);
-            return user;
-        } catch (error) {
-            return null;
-        }
-    }
-
-    isAdmin() {
-        const user = this.getLocalUser();
-        if(user) {
-            return user.role == "admin";
-        }
-
-        return false;
-    }
+    return false;
+  }
 }
